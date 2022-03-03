925059ce5c432df88b9937cac27385b6d1cf05b7
mikiyoshi <mikiyoshi.kokura@gmail.com>
2022-03-03 16:22:14 -0500

    update bind function

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,85 @@
 import React, { Component } from 'react';
 
 import classnames from 'classnames';
+import Loading from './Loading';
+import Panel from './Panel';
+const data = [
+  {
+    id: 1,
+    label: 'Total Interviews',
+    value: 6,
+  },
+  {
+    id: 2,
+    label: 'Least Popular Time Slot',
+    value: '1pm',
+  },
+  {
+    id: 3,
+    label: 'Most Popular Day',
+    value: 'Wednesday',
+  },
+  {
+    id: 4,
+    label: 'Interviews Per Day',
+    value: '2.3',
+  },
+];
 
 class Dashboard extends Component {
-  render() {
-    const dashboardClasses = classnames('dashboard');
+  state = {
+    loading: false,
+  };
+  constructor(props) {
+    //   this is hook
+    super(props); // this is Component (Parent)
+    //..
+    // STATE HERE
+    this.state = {
+      // state is always Object
+      focused: null,
+    };
+    // this.selectPanel = this.selectPanel.bind(this);
+  }
+  // selectPanel = (id) => {
+  //   this.setState((prev) => {
+  //     //   console.log('what is this', prev.likes);
+  //     return {
+  //       focused: id, // クリックした後、再度クリックしても元の４分割に戻らない
+  //     };
+  //   });
+  // };
+  selectPanel(id) {
+    this.setState((previousState) => ({
+      focused: previousState.focused !== null ? null : id,
+    }));
+  }
 
-    return <main className={dashboardClasses} />;
+  render() {
+    const dashboardClasses = classnames('dashboard', {
+      'dashboard--focused': this.state.focused,
+    });
+
+    if (this.state.loading) {
+      return <Loading />;
+    }
+
+    const panels = data
+      .filter(
+        (panel) =>
+          this.state.focused === null || this.state.focused === panel.id
+      )
+      .map((panel) => (
+        <Panel
+          key={panel.id}
+          id={panel.id}
+          label={panel.label}
+          value={panel.value}
+          onSelect={(event) => this.selectPanel(panel.id)}
+        />
+      ));
+
+    return <main className={dashboardClasses}>{panels}</main>;
   }
 }
 
